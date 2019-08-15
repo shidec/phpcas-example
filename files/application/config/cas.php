2f29a57d7cc91aa7c21ff066e10dfc459fcb6b18
Soesapto Joeni Hantoro <shidec00@yahoo.com>
2019-08-15 09:36:10 +0700

    Init

---
+++ files/application/config/cas.php
@@ -0,0 +1,7 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+$config['cas_server_url'] = 'CAS_SERVER_URL';
+$config['phpcas_path'] = 'PHPCAS_PATH';
+$config['cas_disable_server_validation'] = TRUE;
+$config['cas_debug'] = TRUE;
+$config['changeSessionId'] = FALSE;
